ca13b8fc945e9e0ab23e59ab36b776ddbb0535ca
Daniel M <daniel.oliveira1401@outlook.com>
2020-12-18 18:28:07 -0300

    Fixed port number assignment

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 //use the hosting service port or the port 3000 for local hosting
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, (error) => {
     if (error) {
